06a690b4e4e8f7b48e19ebdd319886afbbecef6c
lukyanovpavel <pavel.lukyanovv@gmail.com>
2021-03-12 22:12:18 +0300

    add model, view model, factory

--- app/src/main/java/com/exceptioncatchers/bookfinder/bookdetails/FragmentBookDetails.kt
+++ app/src/main/java/com/exceptioncatchers/bookfinder/bookdetails/FragmentBookDetails.kt
@@ -5,15 +5,53 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.exceptioncatchers.bookfinder.R
+import com.exceptioncatchers.bookfinder.bookdetails.models.BookDetails
+import com.exceptioncatchers.bookfinder.bookdetails.viewmodel.BookDetailsViewModel
+import com.exceptioncatchers.bookfinder.bookdetails.viewmodel.BookDetailsViewModelFactory
 import com.exceptioncatchers.bookfinder.databinding.FragmentBookDetailsBinding
+import kotlinx.android.synthetic.main.fragment_book_details.*
 
 class FragmentBookDetails : Fragment(R.layout.fragment_book_details) {
     private val binding: FragmentBookDetailsBinding by lazy {
         FragmentBookDetailsBinding.inflate(layoutInflater)
     }
+    private val bookDetailsViewModel: BookDetailsViewModel by viewModels {
+        BookDetailsViewModelFactory()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        subscribeBookDetailsResponse()
+    }
+
+    private fun subscribeBookDetailsResponse() {
+        bookDetailsViewModel.getBookDetails()
+            .observe(this.viewLifecycleOwner, { setupBookDetails(it) })
+    }
+
+    private fun setupBookDetails(book: BookDetails) {
+        binding.bookTitle.append(book.bookTitle)
+        binding.bookAutor.append(book.bookAutor)
+        this.context?.let {
+            Glide.with(it)
+                .asBitmap()
+                .load(book.bookPoster)
+                .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+                .centerCrop()
+                .into(binding.bookPoster)
+        }
+        binding.ratingBar.rating = book.bookRating
+        binding.cardViewButton.setOnClickListener { TODO() }
+        binding.bookDescription.append(book.bookDescription)
+        //реализовать диалог с полным описанием по клику
+        binding.bookDescription.setOnClickListener { TODO() }
+        //реализовать подгрузку юзернейма и листенер с переходом в профиль
+        binding.bookOwnerUsername.append(TODO())
+        binding.bookOwnerUsername.setOnClickListener { TODO() }
+        binding.quantityCount.append(book.sharingCount.toString())
     }
 }
