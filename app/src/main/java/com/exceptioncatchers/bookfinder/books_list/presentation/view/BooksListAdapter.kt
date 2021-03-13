e3343daa9b25c6b1ddd37d9ce602551cb9cd773d
Kaharman <kaha_94@mail.ru>
2021-03-13 14:22:42 +0600

    Add CustomRatingBar

--- app/src/main/java/com/exceptioncatchers/bookfinder/books_list/presentation/view/BooksListAdapter.kt
+++ app/src/main/java/com/exceptioncatchers/bookfinder/books_list/presentation/view/BooksListAdapter.kt
@@ -6,19 +6,16 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.exceptioncatchers.bookfinder.R
 import com.exceptioncatchers.bookfinder.books_list.presentation.model.BookItem
+import com.exceptioncatchers.bookfinder.databinding.BookItemBinding
 
 class BooksListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val booksList: MutableList<BookItem> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return BooksListViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.book_item,
-                parent,
-                false
-            )
-        )
+
+        val binding = BookItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return BooksListViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
--- app/src/main/java/com/exceptioncatchers/bookfinder/books_list/presentation/view/BooksListViewHolder.kt
+++ app/src/main/java/com/exceptioncatchers/bookfinder/books_list/presentation/view/BooksListViewHolder.kt
@@ -1,24 +1,13 @@
 package com.exceptioncatchers.bookfinder.books_list.presentation.view
 
-import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
-import com.exceptioncatchers.bookfinder.R
 import com.exceptioncatchers.bookfinder.books_list.presentation.model.BookItem
-import kotlinx.android.synthetic.main.book_item.view.*
+import com.exceptioncatchers.bookfinder.databinding.BookItemBinding
 
-class BooksListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-    private val bookTitle: TextView = view.findViewById(R.id.book_item_title)
-    private val bookAuthor: TextView = view.findViewById(R.id.book_item_author)
-    private val bookImage: ImageView = view.findViewById(R.id.book_item_image)
+class BooksListViewHolder(private val binding: BookItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(bookItem: BookItem) {
-        bookTitle.text = bookItem.title
-        bookAuthor.text = bookItem.author
+        binding.bookItemTitle.text = bookItem.title
+        binding.bookItemAuthor.text = bookItem.author
     }
-
 }
